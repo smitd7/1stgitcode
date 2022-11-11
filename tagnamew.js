6b3402fe67f9f9ad1d8b2bf210819f31b17c75e1
smitd7 <117810432+smitd7@users.noreply.github.com>
2022-11-11 13:33:09 +0530

    Add files via upload

---
+++ tagnamew.js
@@ -0,0 +1,11 @@
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+
+for(for var i=0;i<li.length;i++);{
+    li[i].style.backgroundColor='#f4f4f4';
+
+}
